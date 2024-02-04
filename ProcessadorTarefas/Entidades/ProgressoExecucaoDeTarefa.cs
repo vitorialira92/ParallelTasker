43b093c25d349ac65b3945104b805748c6362fa8
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-02-03 22:12:37 -0300

    feat: classe para entregar informações sobre o progresso da execução da tarefa

---
+++ ProcessadorTarefas/Entidades/ProgressoExecucaoDeTarefa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessadorTarefas.Entidades
+{
+    public class ProgressoExecucaoDeTarefa
+    {
+        public int SubtarefasExecutadas { get; }
+        public int TotalDeSubtarefas { get; }
+
+        public ProgressoExecucaoDeTarefa(int subtarefasExecutadas, int totalDeSubtarefas)
+        {
+            SubtarefasExecutadas = subtarefasExecutadas;
+            TotalDeSubtarefas = totalDeSubtarefas;
+        }
+    }
+}
