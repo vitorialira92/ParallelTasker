7f2415e5a3c26f8ebadce0a1cdc5fb4cbbcc0bfd
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-02-03 22:12:04 -0300

    feat: métodos de extensão para estado de tarefa

---
+++ ProcessadorTarefas/Entidades/EstadoTarefaExtension.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessadorTarefas.Entidades
+{
+    public static class EstadoTarefaExtension
+    {
+        public static bool EhEstadoAtivo(this EstadoTarefa estado)
+        {
+            List<EstadoTarefa> estadosNaoAtivos = new List<EstadoTarefa>()
+            {
+                EstadoTarefa.Concluida,
+                EstadoTarefa.Cancelada
+            };
+
+            return !estadosNaoAtivos.Contains(estado);
+        }
+        public static bool EPossivelCancelar(this EstadoTarefa estado)
+        {
+            List<EstadoTarefa> possiveisParaCancelar = new List<EstadoTarefa>()
+            {
+                EstadoTarefa.Criada,
+                EstadoTarefa.Agendada,
+                EstadoTarefa.EmExecucao
+            };
+
+            return possiveisParaCancelar.Contains(estado);
+        }
+
+        public static bool EPossivelExecutar(this EstadoTarefa estado)
+        {
+            List<EstadoTarefa> possiveisParaExecutar = new List<EstadoTarefa>()
+            {
+                EstadoTarefa.Agendada,
+                EstadoTarefa.EmPausa
+            };
+            return possiveisParaExecutar.Contains(estado);
+        }
+        
+        public static bool EPossivelConcluir(this EstadoTarefa estado)
+        {
+            return estado == EstadoTarefa.EmExecucao;
+        }
+    }
+}
